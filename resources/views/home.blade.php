3b4b9094f1ddfe9bcaf11aa9d5f376aecb23f4ad
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-09 21:12:25 +0300

    added a form with random amount and a button type submit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">Make Payement</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <form action="" method="POST" id="paymentForm">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
 
-                    {{ __('You are logged in!') }}
+                                <label for="amount">How much do you want to pay?</label>
+                                <input type="number" name="value" class="form-control" min="5" step="0.01" value="{{ mt_rand(5,10000)}}" id="">
+
+                            </div>
+
+                        </div>
+                        <div class="text-center mt-3">
+                            <button type="submit" class="btn btn-primary" id="payButton">Pay</button>
+
+                        </div>
+
+
+                    </form>
+
                 </div>
+
+
             </div>
         </div>
     </div>
