e38400ffd14c91c0156e1a2d5b794e071f8a8b3a
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-09 20:12:09 +0300

    changed the primary key as iso and turned auto increament off

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Currency extends Model
 {
     use HasFactory;
+
+    protected $primaryky = 'iso';
+    public $incrementing = false;
+    protected $fillable = [
+        'iso',
+    ]
 }
--- database/migrations/2021_08_09_165519_create_payment_platforms_table.php
+++ database/migrations/2021_08_09_165519_create_payment_platforms_table.php
@@ -15,6 +15,8 @@ class CreatePaymentPlatformsTable extends Migration
     {
         Schema::create('payment_platforms', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('image');
             $table->timestamps();
         });
     }
