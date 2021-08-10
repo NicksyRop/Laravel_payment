ba33ac1507248dab6754755d967e132ebf572478
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-10 08:56:08 +0300

    added validation rules to form in the payemntController

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Currency;
+use App\Models\PaymentPlatform;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function pay(Request $request)
+    {
+        $request->validate([
+
+            'value'=> 'required|min:5|numeric',
+            'currency' => 'required|exists:currencies,iso',
+            'payment_platform' => 'required|exist:payment_platform,id'
+        ]);
+
+    }
+}
